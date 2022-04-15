acbdb535525c7abdde25c709666702ec49060ba3
Josef Erben <josef@erben.io>
2022-04-15 19:16:42 +0200

    Set up assets

---
+++ game/static/assets/map/nature.tsx
@@ -0,0 +1,298 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.5" tiledversion="1.5.0" name="nature" tilewidth="16" tileheight="16" tilecount="71" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <transformations hflip="1" vflip="1" rotate="1" preferuntransformed="1"/>
+ <tile id="0">
+  <image width="16" height="16" source="../tiles/ground/dirt_1.png"/>
+ </tile>
+ <tile id="1">
+  <image width="16" height="16" source="../tiles/ground/dirt_2.png"/>
+ </tile>
+ <tile id="2">
+  <image width="16" height="16" source="../tiles/ground/dirt_3.png"/>
+ </tile>
+ <tile id="3">
+  <image width="16" height="16" source="../tiles/ground/dirt_4.png"/>
+ </tile>
+ <tile id="4">
+  <image width="16" height="16" source="../tiles/ground/dirt_dirt_dirt_water.png"/>
+ </tile>
+ <tile id="5">
+  <image width="16" height="16" source="../tiles/ground/dirt_dirt_water_dirt.png"/>
+ </tile>
+ <tile id="6">
+  <image width="16" height="16" source="../tiles/ground/dirt_dirt_water_water.png"/>
+ </tile>
+ <tile id="7">
+  <image width="16" height="16" source="../tiles/ground/dirt_water_dirt_dirt.png"/>
+ </tile>
+ <tile id="8">
+  <image width="16" height="16" source="../tiles/ground/dirt_water_dirt_water.png"/>
+ </tile>
+ <tile id="9">
+  <image width="16" height="16" source="../tiles/ground/dirt_water_water_dirt.png"/>
+ </tile>
+ <tile id="10">
+  <image width="16" height="16" source="../tiles/ground/dirt_water_water_water.png"/>
+ </tile>
+ <tile id="11">
+  <image width="16" height="16" source="../tiles/ground/grass_1.png"/>
+ </tile>
+ <tile id="12">
+  <image width="16" height="16" source="../tiles/ground/grass_2.png"/>
+ </tile>
+ <tile id="13">
+  <image width="16" height="16" source="../tiles/ground/grass_3.png"/>
+ </tile>
+ <tile id="14">
+  <image width="16" height="16" source="../tiles/ground/grass_grass_grass_jungle.png"/>
+ </tile>
+ <tile id="15">
+  <image width="16" height="16" source="../tiles/ground/grass_grass_grass_water.png"/>
+ </tile>
+ <tile id="16">
+  <image width="16" height="16" source="../tiles/ground/grass_grass_jungle_grass.png"/>
+ </tile>
+ <tile id="17">
+  <image width="16" height="16" source="../tiles/ground/grass_grass_jungle_jungle.png"/>
+ </tile>
+ <tile id="18">
+  <image width="16" height="16" source="../tiles/ground/grass_grass_water_grass.png"/>
+ </tile>
+ <tile id="19">
+  <image width="16" height="16" source="../tiles/ground/grass_grass_water_water.png"/>
+ </tile>
+ <tile id="20">
+  <image width="16" height="16" source="../tiles/ground/grass_jungle_grass_grass.png"/>
+ </tile>
+ <tile id="21">
+  <image width="16" height="16" source="../tiles/ground/grass_jungle_grass_jungle.png"/>
+ </tile>
+ <tile id="22">
+  <image width="16" height="16" source="../tiles/ground/grass_jungle_jungle_grass.png"/>
+ </tile>
+ <tile id="23">
+  <image width="16" height="16" source="../tiles/ground/grass_jungle_jungle_jungle.png"/>
+ </tile>
+ <tile id="24">
+  <image width="16" height="16" source="../tiles/ground/grass_water_grass_grass.png"/>
+ </tile>
+ <tile id="25">
+  <image width="16" height="16" source="../tiles/ground/grass_water_grass_water.png"/>
+ </tile>
+ <tile id="26">
+  <image width="16" height="16" source="../tiles/ground/grass_water_water_grass.png"/>
+ </tile>
+ <tile id="27">
+  <image width="16" height="16" source="../tiles/ground/grass_water_water_water.png"/>
+ </tile>
+ <tile id="28">
+  <image width="16" height="16" source="../tiles/ground/jungle_1.png"/>
+ </tile>
+ <tile id="29">
+  <image width="16" height="16" source="../tiles/ground/jungle_2.png"/>
+ </tile>
+ <tile id="30">
+  <image width="16" height="16" source="../tiles/ground/jungle_3.png"/>
+ </tile>
+ <tile id="31">
+  <image width="16" height="16" source="../tiles/ground/jungle_grass_grass_grass.png"/>
+ </tile>
+ <tile id="32">
+  <image width="16" height="16" source="../tiles/ground/jungle_grass_grass_jungle.png"/>
+ </tile>
+ <tile id="33">
+  <image width="16" height="16" source="../tiles/ground/jungle_grass_jungle_grass.png"/>
+ </tile>
+ <tile id="34">
+  <image width="16" height="16" source="../tiles/ground/jungle_grass_jungle_jungle.png"/>
+ </tile>
+ <tile id="35">
+  <image width="16" height="16" source="../tiles/ground/jungle_jungle_grass_grass.png"/>
+ </tile>
+ <tile id="36">
+  <image width="16" height="16" source="../tiles/ground/jungle_jungle_grass_jungle.png"/>
+ </tile>
+ <tile id="37">
+  <image width="16" height="16" source="../tiles/ground/jungle_jungle_jungle_grass.png"/>
+ </tile>
+ <tile id="38">
+  <image width="16" height="16" source="../tiles/ground/jungle_jungle_jungle_water.png"/>
+ </tile>
+ <tile id="39">
+  <image width="16" height="16" source="../tiles/ground/jungle_jungle_water_jungle.png"/>
+ </tile>
+ <tile id="40">
+  <image width="16" height="16" source="../tiles/ground/jungle_jungle_water_water.png"/>
+ </tile>
+ <tile id="41">
+  <image width="16" height="16" source="../tiles/ground/jungle_water_jungle_jungle.png"/>
+ </tile>
+ <tile id="42">
+  <image width="16" height="16" source="../tiles/ground/jungle_water_jungle_water.png"/>
+ </tile>
+ <tile id="43">
+  <image width="16" height="16" source="../tiles/ground/jungle_water_water_jungle.png"/>
+ </tile>
+ <tile id="44">
+  <image width="16" height="16" source="../tiles/ground/jungle_water_water_water.png"/>
+ </tile>
+ <tile id="45">
+  <image width="16" height="16" source="../tiles/ground/lake_1.png"/>
+ </tile>
+ <tile id="46">
+  <image width="16" height="16" source="../tiles/ground/lake_2.png"/>
+ </tile>
+ <tile id="50">
+  <image width="16" height="16" source="../tiles/ground/sand_1.png"/>
+ </tile>
+ <tile id="51">
+  <image width="16" height="16" source="../tiles/ground/sand_2.png"/>
+ </tile>
+ <tile id="52">
+  <image width="16" height="16" source="../tiles/ground/sand_3.png"/>
+ </tile>
+ <tile id="53">
+  <image width="16" height="16" source="../tiles/ground/water_dirt_dirt_dirt.png"/>
+ </tile>
+ <tile id="54">
+  <image width="16" height="16" source="../tiles/ground/water_dirt_dirt_water.png"/>
+ </tile>
+ <tile id="55">
+  <image width="16" height="16" source="../tiles/ground/water_dirt_water_dirt.png"/>
+ </tile>
+ <tile id="56">
+  <image width="16" height="16" source="../tiles/ground/water_dirt_water_water.png"/>
+ </tile>
+ <tile id="57">
+  <image width="16" height="16" source="../tiles/ground/water_grass_grass_grass.png"/>
+ </tile>
+ <tile id="58">
+  <image width="16" height="16" source="../tiles/ground/water_grass_grass_water.png"/>
+ </tile>
+ <tile id="59">
+  <image width="16" height="16" source="../tiles/ground/water_grass_water_grass.png"/>
+ </tile>
+ <tile id="60">
+  <image width="16" height="16" source="../tiles/ground/water_grass_water_water.png"/>
+ </tile>
+ <tile id="61">
+  <image width="16" height="16" source="../tiles/ground/water_jungle_jungle_jungle.png"/>
+ </tile>
+ <tile id="62">
+  <image width="16" height="16" source="../tiles/ground/water_jungle_jungle_water.png"/>
+ </tile>
+ <tile id="63">
+  <image width="16" height="16" source="../tiles/ground/water_jungle_water_jungle.png"/>
+ </tile>
+ <tile id="64">
+  <image width="16" height="16" source="../tiles/ground/water_jungle_water_water.png"/>
+ </tile>
+ <tile id="65">
+  <image width="16" height="16" source="../tiles/ground/water_water_dirt_dirt.png"/>
+ </tile>
+ <tile id="66">
+  <image width="16" height="16" source="../tiles/ground/water_water_dirt_water.png"/>
+ </tile>
+ <tile id="67">
+  <image width="16" height="16" source="../tiles/ground/water_water_grass_grass.png"/>
+ </tile>
+ <tile id="68">
+  <image width="16" height="16" source="../tiles/ground/water_water_grass_water.png"/>
+ </tile>
+ <tile id="69">
+  <image width="16" height="16" source="../tiles/ground/water_water_jungle_jungle.png"/>
+ </tile>
+ <tile id="70">
+  <image width="16" height="16" source="../tiles/ground/water_water_jungle_water.png"/>
+ </tile>
+ <tile id="71">
+  <image width="16" height="16" source="../tiles/ground/water_water_water_dirt.png"/>
+ </tile>
+ <tile id="72">
+  <image width="16" height="16" source="../tiles/ground/water_water_water_grass.png"/>
+ </tile>
+ <tile id="73">
+  <image width="16" height="16" source="../tiles/ground/water_water_water_jungle.png"/>
+ </tile>
+ <wangsets>
+  <wangset name="ground" type="corner" tile="-1">
+   <wangcolor name="grass" color="#00ff00" tile="-1" probability="1"/>
+   <wangcolor name="water" color="#0000ff" tile="-1" probability="1"/>
+   <wangcolor name="dirt" color="#ff7700" tile="-1" probability="1"/>
+   <wangcolor name="sand" color="#cdab8f" tile="-1" probability="1"/>
+   <wangcolor name="jungle" color="#26a269" tile="-1" probability="1"/>
+   <wangtile tileid="0" wangid="0,3,0,3,0,3,0,3"/>
+   <wangtile tileid="1" wangid="0,3,0,3,0,3,0,3"/>
+   <wangtile tileid="2" wangid="0,3,0,3,0,3,0,3"/>
+   <wangtile tileid="3" wangid="0,3,0,3,0,3,0,3"/>
+   <wangtile tileid="4" wangid="0,3,0,3,0,2,0,3"/>
+   <wangtile tileid="5" wangid="0,3,0,2,0,3,0,3"/>
+   <wangtile tileid="6" wangid="0,3,0,2,0,2,0,3"/>
+   <wangtile tileid="7" wangid="0,2,0,3,0,3,0,3"/>
+   <wangtile tileid="8" wangid="0,2,0,3,0,2,0,3"/>
+   <wangtile tileid="9" wangid="0,2,0,2,0,3,0,3"/>
+   <wangtile tileid="10" wangid="0,2,0,2,0,2,0,3"/>
+   <wangtile tileid="11" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="12" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="13" wangid="0,1,0,1,0,1,0,1"/>
+   <wangtile tileid="14" wangid="0,1,0,1,0,5,0,1"/>
+   <wangtile tileid="15" wangid="0,1,0,1,0,2,0,1"/>
+   <wangtile tileid="16" wangid="0,1,0,5,0,1,0,1"/>
+   <wangtile tileid="17" wangid="0,1,0,5,0,5,0,1"/>
+   <wangtile tileid="18" wangid="0,1,0,2,0,1,0,1"/>
+   <wangtile tileid="19" wangid="0,1,0,2,0,2,0,1"/>
+   <wangtile tileid="20" wangid="0,5,0,1,0,1,0,1"/>
+   <wangtile tileid="21" wangid="0,5,0,1,0,5,0,1"/>
+   <wangtile tileid="22" wangid="0,5,0,5,0,1,0,1"/>
+   <wangtile tileid="23" wangid="0,5,0,5,0,5,0,1"/>
+   <wangtile tileid="24" wangid="0,2,0,1,0,1,0,1"/>
+   <wangtile tileid="25" wangid="0,2,0,1,0,2,0,1"/>
+   <wangtile tileid="26" wangid="0,2,0,2,0,1,0,1"/>
+   <wangtile tileid="27" wangid="0,2,0,2,0,2,0,1"/>
+   <wangtile tileid="28" wangid="0,5,0,5,0,5,0,5"/>
+   <wangtile tileid="29" wangid="0,5,0,5,0,5,0,5"/>
+   <wangtile tileid="30" wangid="0,5,0,5,0,5,0,5"/>
+   <wangtile tileid="31" wangid="0,1,0,1,0,1,0,5"/>
+   <wangtile tileid="32" wangid="0,1,0,1,0,5,0,5"/>
+   <wangtile tileid="33" wangid="0,1,0,5,0,1,0,5"/>
+   <wangtile tileid="34" wangid="0,1,0,5,0,5,0,5"/>
+   <wangtile tileid="35" wangid="0,5,0,1,0,1,0,5"/>
+   <wangtile tileid="36" wangid="0,5,0,1,0,5,0,5"/>
+   <wangtile tileid="37" wangid="0,5,0,5,0,1,0,5"/>
+   <wangtile tileid="38" wangid="0,5,0,5,0,2,0,5"/>
+   <wangtile tileid="39" wangid="0,5,0,2,0,5,0,5"/>
+   <wangtile tileid="40" wangid="0,5,0,2,0,2,0,5"/>
+   <wangtile tileid="41" wangid="0,2,0,5,0,5,0,5"/>
+   <wangtile tileid="42" wangid="0,2,0,5,0,2,0,5"/>
+   <wangtile tileid="43" wangid="0,2,0,2,0,5,0,5"/>
+   <wangtile tileid="44" wangid="0,2,0,2,0,2,0,5"/>
+   <wangtile tileid="45" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="46" wangid="0,2,0,2,0,2,0,2"/>
+   <wangtile tileid="50" wangid="0,4,0,4,0,4,0,4"/>
+   <wangtile tileid="51" wangid="0,4,0,4,0,4,0,4"/>
+   <wangtile tileid="52" wangid="0,4,0,4,0,4,0,4"/>
+   <wangtile tileid="53" wangid="0,3,0,3,0,3,0,2"/>
+   <wangtile tileid="54" wangid="0,3,0,3,0,2,0,2"/>
+   <wangtile tileid="55" wangid="0,3,0,2,0,3,0,2"/>
+   <wangtile tileid="56" wangid="0,3,0,2,0,2,0,2"/>
+   <wangtile tileid="57" wangid="0,1,0,1,0,1,0,2"/>
+   <wangtile tileid="58" wangid="0,1,0,1,0,2,0,2"/>
+   <wangtile tileid="59" wangid="0,1,0,2,0,1,0,2"/>
+   <wangtile tileid="60" wangid="0,1,0,2,0,2,0,2"/>
+   <wangtile tileid="61" wangid="0,5,0,5,0,5,0,2"/>
+   <wangtile tileid="62" wangid="0,5,0,5,0,2,0,2"/>
+   <wangtile tileid="63" wangid="0,5,0,2,0,5,0,2"/>
+   <wangtile tileid="64" wangid="0,5,0,2,0,2,0,2"/>
+   <wangtile tileid="65" wangid="0,2,0,3,0,3,0,2"/>
+   <wangtile tileid="66" wangid="0,2,0,3,0,2,0,2"/>
+   <wangtile tileid="67" wangid="0,2,0,1,0,1,0,2"/>
+   <wangtile tileid="68" wangid="0,2,0,1,0,2,0,2"/>
+   <wangtile tileid="69" wangid="0,2,0,5,0,5,0,2"/>
+   <wangtile tileid="70" wangid="0,2,0,5,0,2,0,2"/>
+   <wangtile tileid="71" wangid="0,2,0,2,0,3,0,2"/>
+   <wangtile tileid="72" wangid="0,2,0,2,0,1,0,2"/>
+   <wangtile tileid="73" wangid="0,2,0,2,0,5,0,2"/>
+  </wangset>
+ </wangsets>
+</tileset>
