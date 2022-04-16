80ed29084ca4eba686d0fce72af6af12f31118a1
Josef Erben <josef@erben.io>
2022-04-16 09:57:03 +0200

    Fix linting issues

--- game/users/urls.py
+++ game/users/urls.py
@@ -1,10 +1,6 @@
 from django.urls import path
 
-from game.users.views import (
-    user_detail_view,
-    user_redirect_view,
-    user_update_view,
-)
+from game.users.views import user_detail_view, user_redirect_view, user_update_view
 
 app_name = "users"
 urlpatterns = [
