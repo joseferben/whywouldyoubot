238cbc771e86e4d284a4e0629481028afe6c8275
Josef Erben <josef@erben.io>
2022-05-18 15:18:24 +0200

    Improve documentation

--- lib/app.ml
+++ lib/app.ml
@@ -2,18 +2,28 @@ module Config = (val !Settings.config : Settings.S)
 
 let routes =
   [ Dream.get "/" Handlers.home
-  ; Dream.scope "/users" [] Sihl.User.routes
   ; Dream.scope
       "/orders"
-      [ Sihl.User.authentication_required ]
-      [ Dream.get "/" Handlers.order_list
-      ; Dream.get "/:id" Handlers.order_detail
-      ; Dream.get "/create" Handlers.order_create
+      [ Sihl.User.is_authenticated ]
+      [ Sihl.User.is_staff @@ Dream.get "/" Handlers.order_list
+      ; Sihl.Model.single_object ~pk:"id"
+        @@ Sihl.User.passes_test Customer.owns_order
+        @@ Dream.get "/:id" Handlers.order_detail
+      ; Sihl.Model.single_object ~pk:"id"
+        @@ Sihl.User.passes_test Customer.owns_order
+        @@ Dream.delete "/:id" Handlers.order_delete
+      ; Dream.scope "/create" Handlers.order_create
       ]
+  ; Dream.scope
+      "/customers"
+      [ Sihl.User.is_authenticated; Sihl.User.is_superuser ]
+      [ Dream.get "/" Handlers.customer_list ]
   ]
 ;;
 
 let run () =
-  Sihl.User.configure !Settings.config;
-  Dream.run ~interface:"0.0.0.0" @@ Config.middlewares @@ Dream.router routes
+  Sihl.run (module Config)
+  @@ Dream.serve ~interface:"0.0.0.0"
+  @@ Config.middlewares
+  @@ Dream.router routes
 ;;
