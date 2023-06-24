1bdeb8901467ef5f6bb47507f13c744a98b313f1
Josef Erben <josef@erben.systems>
2023-06-24 18:45:28 +0200

    new remix app

---
+++ remix.config.js
@@ -0,0 +1,15 @@
+/** @type {import('@remix-run/dev').AppConfig} */
+module.exports = {
+  cacheDirectory: "./node_modules/.cache/remix",
+  future: {
+    v2_errorBoundary: true,
+    v2_headers: true,
+    v2_meta: true,
+    v2_normalizeFormMethod: true,
+    v2_routeConvention: true,
+  },
+  ignoredRouteFiles: ["**/.*", "**/*.test.{js,jsx,ts,tsx}"],
+  postcss: true,
+  serverModuleFormat: "cjs",
+  tailwind: true,
+};
