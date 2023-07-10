ae2e1ed3041771efdf13e4b240f7ea605a9d6651
Josef Erben <josef@erben.systems>
2023-07-10 21:51:46 +0200

    fix type error

--- apps/game/app/routes/logout.tsx
+++ apps/game/app/routes/logout.tsx
@@ -3,15 +3,13 @@ import { redirect } from "@remix-run/node";
 import { container } from "~/container.server";
 
 export const action: ActionFunction = async ({ request }) => {
-  const player = await container.sessionService.requirePlayer(request);
-  if (!player) return redirect("/");
+  const player = await container.authService.ensurePlayer(request);
   console.debug(`player ${player.username} logs out`);
   return container.sessionService.logout(request);
 };
 
 export const loader: LoaderFunction = async ({ request }) => {
-  const player = await container.sessionService.requirePlayer(request);
-  if (!player) return redirect("/");
+  const player = await container.authService.ensurePlayer(request);
   console.debug(`player ${player.username} logs out`);
   return container.sessionService.logout(request);
 };
