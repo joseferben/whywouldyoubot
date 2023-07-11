a5aedaaecc46bfe98d27677c07e46e6350f8acbd
Josef Erben <josef@erben.systems>
2023-07-11 21:08:17 +0200

    redirect to /game

--- apps/game/app/routes/auth.discord.tsx
+++ apps/game/app/routes/auth.discord.tsx
@@ -3,7 +3,10 @@ import { redirect } from "@remix-run/node";
 import { container } from "~/container.server";
 
 export const action: ActionFunction = async ({ request }) => {
-  return container.authService.authenticate(request);
+  return container.authService.auth.authenticate("discord", request, {
+    successRedirect: "/game",
+    failureRedirect: "/",
+  });
 };
 
 export let loader: LoaderFunction = () => redirect("/");
