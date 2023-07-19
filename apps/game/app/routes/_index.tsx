9ba7488e3b24e079f23102f27f92522b542f4120
Josef Erben <josef@erben.systems>
2023-07-19 23:23:23 +0200

    fix linting

--- apps/game/app/routes/_index.tsx
+++ apps/game/app/routes/_index.tsx
@@ -6,7 +6,6 @@ import { StoreContext, createGameStore } from "~/store";
 import { DOMRenderer } from "~/components/DOMRenderer";
 import { Menu } from "~/components/Menu";
 import { EventSource } from "~/components/EventSource";
-import { config } from "~/config";
 
 export const loader = async ({ request }: LoaderArgs) => {
   const player = await container.authService.ensurePlayer(request);
